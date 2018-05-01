e9126ca290f7954d49ab809d018339fee028fa11
heiang <598481337@qq.com>
2018-05-01 23:36:22 +0800

    提交代码

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -13,12 +13,50 @@ Page({
     focus:false,
     ary:[],
     token: null,
-    isZan:false
+    isZan:false,
+    faceShow:false,
+    inputEvent:null,
+    pos:0
   },
   //输入文字
   inputPublish: function(e) {
+    let pos = e.detail.cursor;
     this.setData({
-      publishTxt: e.detail.value
+      publishTxt: e.detail.value,
+      inputEvent:e,
+      pos
+    })
+   
+  },
+  focusFn(e){
+    console.log(`focusFn`);
+    this.setData({
+      inputEvent: e
+    })
+  },
+  //插入表情
+  insertSmail(e){
+    
+    let dataset = e.currentTarget.dataset;
+    let { inputEvent, pos}=this.data;
+    let smailFlag = dataset.val;
+    // console.log(inputEvent);
+    let value= inputEvent.detail.value;
+    // let pos = inputEvent.detail.cursor;
+    console.log(inputEvent);
+
+    let left = value.substr(0, pos);
+    console.log("pos:"+pos);
+    console.log("value:"+value);
+    console.log("left:" + left);
+    let right = value.substr(pos, value.length);
+    console.log("right:" + right);
+    let result = left + smailFlag + right;
+
+    this.setData({
+      publishTxt: result,
+      focus: true,
+      faceShow: false
     })
   },
   replay:function(e){
@@ -31,7 +69,14 @@ Page({
       pid: uuid
     })
   },
-
+  //打开表情窗口
+  openFaceDialog(){
+    let { faceShow}=this.data;
+     this.setData({
+       faceShow: !faceShow,
+       focus: true
+     })
+  },
   //设置当前为评论状态
   setComment(e){
     this.setData({
@@ -44,6 +89,9 @@ Page({
 
   //发送评论
   send:function(){
+    this.setData({
+      faceShow: false
+    })
     let { publishTxt, blog, type,pid}=this.data;
     let { userInfo}=app.globalData;
      let comment={};
@@ -91,11 +139,18 @@ Page({
   // 获取评论列表
  getComments(isShow){
    let { blog } = this.data;
+   let that=this;
    isShow=isShow==null?true:false;
    fetch.get(`comment/getComments/${blog.uuid}`, isShow).then((data) => {
       this.setData({
         ary: data
       })
+      //  console.log()
+      //  data.forEach((item,index)=>{
+      //   //  console.log(item.content);
+      //    WxParse.wxParse('comment_' + index, 'html', item.content, that,5);
+      //  })
+       // WxParse.wxParse('content', 'html', data.module.content, that,5);
     })
  },
 
